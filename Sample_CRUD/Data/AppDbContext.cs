39e4fb46a732b81ce707a584833cd8676d0997b4
Nikalya_1398 <v.ninmathirasa@gmail.com>
2024-06-27 16:00:26 +0530

    Add project files.

---
+++ Sample_CRUD/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sample_CRUD.Models;
+
+namespace Sample_CRUD.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+
+   
+}
